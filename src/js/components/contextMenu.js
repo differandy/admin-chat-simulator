3f4f23c16a51773eb478f8ed1c10c75907c6ecfa
differandy <differandies@gmail.com>
2024-02-29 23:29:38 +0500

    feat: add basic template for contextMenu

--- src/js/components/contextMenu.js
+++ src/js/components/contextMenu.js
@@ -1,5 +1,17 @@
 const createContextMenu = () => {
 	return `
-  
+    <nav class="context-menu">
+      <ul class="context-menu__list">
+        <li class="context-menu__item">
+          <a href="#" class="context-menu__link">Edit message</a>        
+        </li>
+        <li class="context-menu__item">
+          <a href="#" class="context-menu__link">Delete message</a>        
+        </li>
+        <li class="context-menu__item">
+          <a href="#" class="context-menu__link">Mute</a>        
+        </li>
+      </ul>
+    </nav>
   `;
 };
